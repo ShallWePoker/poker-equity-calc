a8d77eec9bf8548a0181f3d4d1bab60680d438fd
chinabobo <632996201@qq.com>
2022-04-07 14:48:00 +0800

    feat : add GenerateAllHandFrom2Ranks

--- internal/consts/titles_suits.go
+++ internal/consts/titles_suits.go
@@ -6,3 +6,23 @@ var SuitsValid = map[string]string{
 	"♠": "S",
 	"♣": "C",
 }
+
+var Suits = []string{
+	"H", "D", "S", "C",
+}
+
+var ValidTitles = map[string]int{
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+	"T": 10,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+	"A": 14,
+}
