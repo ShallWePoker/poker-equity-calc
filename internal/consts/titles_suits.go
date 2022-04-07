1ba0118ea576cdf2d0a4da00ae110713693301da
githcorrado <githcorrado@gmail.com>
2022-04-07 14:20:46 +0800

    fix: remove unused consts; updated hand2hand equity calculation process

--- internal/consts/titles_suits.go
+++ internal/consts/titles_suits.go
@@ -1,31 +1,8 @@
 package consts
 
-var ValidSuits = map[string]string{
-	"H": "❤",
-	"D": "♦",
-	"S": "♠",
-	"C": "♣",
-}
-
 var SuitsValid = map[string]string{
 	"❤": "H",
 	"♦": "D",
 	"♠": "S",
 	"♣": "C",
-}
-
-var ValidTitles = map[string]int{
-	"2": 2,
-	"3": 3,
-	"4": 4,
-	"5": 5,
-	"6": 6,
-	"7": 7,
-	"8": 8,
-	"9": 9,
-	"T": 10,
-	"J": 11,
-	"Q": 12,
-	"K": 13,
-	"A": 14,
 }
