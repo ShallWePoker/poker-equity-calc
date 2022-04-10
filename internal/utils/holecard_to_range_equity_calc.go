ba43f013a256e52d66aa7d82639280cf48f97118
githcorrado <githcorrado@gmail.com>
2022-04-11 00:12:40 +0800

    feat: + fixed a few bugs relating to ThreeOfAKind and Flush comparison + added hand to range preflop, flop, turn, river equity calc functions and tests + refactored equity calc functions to get rid of repeated codes

--- internal/utils/holecard_to_range_equity_calc.go
+++ internal/utils/holecard_to_range_equity_calc.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"errors"
+	"fmt"
+	"github.com/ShallWePoker/poker-equity-calc/internal/consts"
 	"github.com/ShallWePoker/poker-equity-calc/internal/models"
 )
 
@@ -20,37 +22,28 @@ func HoleCardVersusRangePreflopEquity(hand1 models.HoleCard, hand2Ranges []model
 	hand2Holecards := GenerateAllHoleCardFromMultiRange(hand2Ranges, hand1...)
 
 	for _, hand2Holecard := range hand2Holecards {
+		fmt.Printf("\nhand1 %s vs hand2 %s\n", hand1.ToString(), hand2Holecard.ToString())
 		cardsToRemoveFromDeck := make([]models.Card, 0)
 		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand1...)
 		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand2Holecard...)
 		// TODO seems ExamineDistinctCards unnecessary here;
+		/*
 		err = ExamineDistinctCards(cardsToRemoveFromDeck...)
 		if err != nil {
 			return hand1Equity, hand2RangeEquity, err
 		}
-		defaultSampleSize := 500
+		 */
+		defaultSampleSize := 1000
 		totalSampleSize = totalSampleSize + defaultSampleSize
-		for i := 0; i < defaultSampleSize; i++ {
-			generated5Cards, err := GenerateRandomNCards(5, cardsToRemoveFromDeck)
-			if err != nil {
-				return hand1Equity, hand2RangeEquity, err
-			}
-			hand1BiggestMadeHand := Seven2five(append(generated5Cards, hand1...))
-			hand2BiggestMadeHand := Seven2five(append(generated5Cards, hand2Holecard...))
-			if hand1BiggestMadeHand.IsGreaterThan(hand2BiggestMadeHand) {
-				hand1Win += 1
-			} else if hand2BiggestMadeHand.IsGreaterThan(hand1BiggestMadeHand) {
-				hand2RangesWin += 1
-			} else {
-				tie += 1
-			}
+		err = Hand2HandCountWinAndTie(hand1, hand2Holecard,
+			&hand1Win, &hand2RangesWin, &tie,
+			consts.PREFLOP, cardsToRemoveFromDeck, defaultSampleSize,
+			true, nil, nil, nil)
+		if err != nil {
+			return hand1Equity, hand2RangeEquity, err
 		}
 	}
-	hand1WinRate := hand1Win / float64(totalSampleSize)
-	hand1EquityRate := (hand1Win+tie/float64(2)) / float64(totalSampleSize)
-	hand2RangesWinRate := hand2RangesWin / float64(totalSampleSize)
-	hand2RangesEquityRate := (hand2RangesWin+tie/float64(2)) / float64(totalSampleSize)
-	tieRate := tie / float64(totalSampleSize)
+	hand1WinRate, hand1EquityRate, hand2RangesWinRate, hand2RangesEquityRate, tieRate := Hand2HandRateCalc(hand1Win, hand2RangesWin, tie, float64(totalSampleSize))
 	hand1Equity.Equity = hand1EquityRate
 	hand1Equity.WinRate = hand1WinRate
 	hand1Equity.TieRate = tieRate
@@ -59,3 +52,122 @@ func HoleCardVersusRangePreflopEquity(hand1 models.HoleCard, hand2Ranges []model
 	hand2RangeEquity.TieRate = tieRate
 	return hand1Equity, hand2RangeEquity, nil
 }
+
+func HoleCardVersusRangeFlopEquity(hand1 models.HoleCard, hand2Ranges []models.HoleCardRange, flop models.Flop) (hand1Equity models.HoleCardEquity, hand2RangeEquity models.HoleCardRangeEquity, err error) {
+	if len(hand2Ranges) == 0 {
+		return hand1Equity, hand2RangeEquity, errors.New("hand2 range must not be empty")
+	}
+	hand1Equity.HoleCard = hand1
+	hand2RangeEquity.HoleCardRange = hand2Ranges
+	hand1Win := float64(0)
+	hand2RangesWin := float64(0)
+	tie := float64(0)
+	totalSampleSize := 0
+
+	hand2Holecards := GenerateAllHoleCardFromMultiRange(hand2Ranges, append(flop, hand1...)...)
+
+	for _, hand2Holecard := range hand2Holecards {
+		fmt.Printf("\nhand1 %s vs hand2 %s\n", hand1.ToString(), hand2Holecard.ToString())
+		cardsToRemoveFromDeck := make([]models.Card, 0)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand1...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand2Holecard...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, flop...)
+		defaultSampleSize := 1000
+		totalSampleSize = totalSampleSize + defaultSampleSize
+		err = Hand2HandCountWinAndTie(hand1, hand2Holecard,
+			&hand1Win, &hand2RangesWin, &tie,
+			consts.FLOP, cardsToRemoveFromDeck, defaultSampleSize,
+			true, &flop, nil, nil,
+		)
+	}
+	hand1WinRate, hand1EquityRate, hand2RangesWinRate, hand2RangesEquityRate, tieRate := Hand2HandRateCalc(hand1Win, hand2RangesWin, tie, float64(totalSampleSize))
+	hand1Equity.Equity = hand1EquityRate
+	hand1Equity.WinRate = hand1WinRate
+	hand1Equity.TieRate = tieRate
+	hand2RangeEquity.Equity = hand2RangesEquityRate
+	hand2RangeEquity.WinRate = hand2RangesWinRate
+	hand2RangeEquity.TieRate = tieRate
+	return hand1Equity, hand2RangeEquity, nil
+}
+
+func HoleCardVersusRangeTurnEquity(hand1 models.HoleCard, hand2Ranges []models.HoleCardRange, flop models.Flop, turn models.Card) (hand1Equity models.HoleCardEquity, hand2RangeEquity models.HoleCardRangeEquity, err error) {
+	if len(hand2Ranges) == 0 {
+		return hand1Equity, hand2RangeEquity, errors.New("hand2 range must not be empty")
+	}
+	hand1Equity.HoleCard = hand1
+	hand2RangeEquity.HoleCardRange = hand2Ranges
+	hand1Win := float64(0)
+	hand2RangesWin := float64(0)
+	tie := float64(0)
+	totalSampleSize := 0
+
+	hand2Holecards := GenerateAllHoleCardFromMultiRange(hand2Ranges, append(append(flop, turn), hand1...)...)
+
+	for _, hand2Holecard := range hand2Holecards {
+		fmt.Printf("\nhand1 %s vs hand2 %s\n", hand1.ToString(), hand2Holecard.ToString())
+		cardsToRemoveFromDeck := make([]models.Card, 0)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand1...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand2Holecard...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, flop...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, turn)
+		defaultSampleSize := 1000
+		totalSampleSize += defaultSampleSize
+		err = Hand2HandCountWinAndTie(hand1, hand2Holecard,
+			&hand1Win, &hand2RangesWin, &tie,
+			consts.TURN, cardsToRemoveFromDeck, defaultSampleSize,
+			true, &flop, &turn, nil)
+		if err != nil {
+			return hand1Equity, hand2RangeEquity, err
+		}
+	}
+	hand1WinRate, hand1EquityRate, hand2RangesWinRate, hand2RangesEquityRate, tieRate := Hand2HandRateCalc(hand1Win, hand2RangesWin, tie, float64(totalSampleSize))
+	hand1Equity.Equity = hand1EquityRate
+	hand1Equity.WinRate = hand1WinRate
+	hand1Equity.TieRate = tieRate
+	hand2RangeEquity.Equity = hand2RangesEquityRate
+	hand2RangeEquity.WinRate = hand2RangesWinRate
+	hand2RangeEquity.TieRate = tieRate
+	return hand1Equity, hand2RangeEquity, nil
+}
+
+func HoleCardVersusRangeRiverEquity(hand1 models.HoleCard, hand2Ranges []models.HoleCardRange, flop models.Flop, turn models.Card, river models.Card) (hand1Equity models.HoleCardEquity, hand2RangeEquity models.HoleCardRangeEquity, err error) {
+	if len(hand2Ranges) == 0 {
+		return hand1Equity, hand2RangeEquity, errors.New("hand2 range must not be empty")
+	}
+	hand1Equity.HoleCard = hand1
+	hand2RangeEquity.HoleCardRange = hand2Ranges
+	hand1Win := float64(0)
+	hand2RangesWin := float64(0)
+	tie := float64(0)
+	totalSampleSize := 0
+
+	hand2Holecards := GenerateAllHoleCardFromMultiRange(hand2Ranges, append(append(flop, turn, river), hand1...)...)
+
+	for _, hand2Holecard := range hand2Holecards {
+		fmt.Printf("\nhand1 %s vs hand2 %s\n", hand1.ToString(), hand2Holecard.ToString())
+		cardsToRemoveFromDeck := make([]models.Card, 0)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand1...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, hand2Holecard...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, flop...)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, turn)
+		cardsToRemoveFromDeck = append(cardsToRemoveFromDeck, river)
+		defaultSampleSize := 1000
+		totalSampleSize += defaultSampleSize
+		err = Hand2HandCountWinAndTie(hand1, hand2Holecard,
+			&hand1Win, &hand2RangesWin, &tie,
+			consts.RIVER, cardsToRemoveFromDeck, defaultSampleSize,
+			true, &flop, &turn, &river)
+		if err != nil {
+			return hand1Equity, hand2RangeEquity, err
+		}
+	}
+	hand1WinRate, hand1EquityRate, hand2RangesWinRate, hand2RangesEquityRate, tieRate := Hand2HandRateCalc(hand1Win, hand2RangesWin, tie, float64(totalSampleSize))
+	hand1Equity.Equity = hand1EquityRate
+	hand1Equity.WinRate = hand1WinRate
+	hand1Equity.TieRate = tieRate
+	hand2RangeEquity.Equity = hand2RangesEquityRate
+	hand2RangeEquity.WinRate = hand2RangesWinRate
+	hand2RangeEquity.TieRate = tieRate
+	return hand1Equity, hand2RangeEquity, nil
+}
+
